8a7ca1c3fa167376bd7058d6c1b8780f53761dbc
MahtoSunil <sunilmahto806@yahoo.com>
2023-03-23 12:03:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationBar from './NavigationBar';
+import "./App.css";
+import myImage from './image2.jpg';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <NavigationBar />
+      <h1>Welcome to My Website</h1>
+      <img src={myImage} alt="My Image" className="full-width"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavigationBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+const NavigationBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto navbar bg-danger">
+
+          <NavDropdown title="Home" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#home1">Home 1</NavDropdown.Item>
+            <NavDropdown.Item href="#home2">Home 2</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#home3">Home 3</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="About" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#about1">About 1</NavDropdown.Item>
+            <NavDropdown.Item href="#about2">About 2</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#about3">About 3</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="Services" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#service1">Service 1</NavDropdown.Item>
+            <NavDropdown.Item href="#service2">Service 2</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#service3">Service 3</NavDropdown.Item>
+          </NavDropdown>
+
+          <NavDropdown title="Contact" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#contact1">Contact 1</NavDropdown.Item>
+            <NavDropdown.Item href="#contact2">Contact 2</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#contact3">Contact 3</NavDropdown.Item>
+          </NavDropdown>
+
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
